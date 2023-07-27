34769e90671d3b6c7ad28495c88531d572fb6182
aditya1327 <adityamavudi2003@gmail.com>
2023-07-27 18:21:43 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import Hero from './components/Hero';
+import Demo from './components/Demo';
+import './App.css';
+const App = () => {
+  return (
+      <main>
+        <div className="gradient" />
+        <div className="app">
+            <Hero />
+            <Demo />
+        </div>
+      </main>
+  )
+}
+
+export default App
---
+++ src/components/Hero.jsx
@@ -0,0 +1,28 @@
+import React from 'react'
+import { logo } from '../assets';
+const Hero = () => {
+  return (
+    <header className="w-full flex justify-center items-center flex-col">
+    <nav className="flex justify-between items-center w-full mb-10 pt-10 ">
+     <img src={logo} alt="sumz_logo" 
+     className='w-28 object-contain'
+     />
+     <button onClick={()=>window.open('https://github.com/aditya1327/ai-summarizer')} 
+     className="black_btn"
+     >
+        GitHub
+     </button>
+    </nav>
+    <h1 className="head_text">
+        Summerize Articles with <br className='max-md:hidden' />
+        <span className='orange_gradient' > Open AI GPT </span>
+    </h1>
+    <h2 className="desc">
+    Simplify your reading with Summize, an open-source article summarizer
+        that transforms lengthy articles into clear and concise summaries
+    </h2>
+    </header>
+  )
+}
+
+export default Hero
